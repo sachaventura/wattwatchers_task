7a8a036e43d1443d7d448206c09c8b6c5bc520e9
sachaaaaa <sacha@loki.network>
2019-10-30 21:53:24 +1100

    Proof of concept - fetching data from WW server

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
-from flask import Flask
+from flask import Flask, request, Response
+import requests
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Flask Dockerized'
+def fetch_data(api_key, device_id):
+    headers = {
+        'Authorization': 'Bearer %s' % api_key
+    }
+    params={
+        'granularity': '30m'
+    }
+    response = requests.get('https://api-v3.wattwatchers.com.au//long-energy/%s' % device_id, headers=headers, params=params)
+    return response.content
+
+@app.route('/monthly_energy')
+def monthly_energy():
+    api_key = request.args.get('api_key')
+    device_id = request.args.get('device_id')
+    data = fetch_data(api_key, device_id)
+    return Response(data, mimetype='text/plain')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
